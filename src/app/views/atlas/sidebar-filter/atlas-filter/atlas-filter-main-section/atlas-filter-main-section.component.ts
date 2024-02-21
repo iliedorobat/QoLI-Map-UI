0008308845be68306e1802ed90cc4df1080e1f14
Ilie Cristian Dorobat <idorobat@cisco.com>
2024-02-22 00:46:44 +0200

    Improve the filtering feature

--- src/app/views/atlas/sidebar-filter/atlas-filter/atlas-filter-main-section/atlas-filter-main-section.component.ts
+++ src/app/views/atlas/sidebar-filter/atlas-filter/atlas-filter-main-section/atlas-filter-main-section.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatCheckboxModule} from '@angular/material/checkbox';
@@ -18,25 +18,33 @@ import {AVAILABLE_INTERVAL} from '@/app/shared/constants/app.const';
     standalone: true,
     imports: [BrowserAnimationsModule, FormsModule, MatCheckboxModule, MatInputModule, MatSelectModule, ReactiveFormsModule]
 })
-export class AtlasFilterMainSectionComponent {
+export class AtlasFilterMainSectionComponent implements OnInit {
     constructor(
         private atlasFilterService: AtlasFilterService
     ) {}
 
     protected readonly AVAILABLE_INTERVAL = AVAILABLE_INTERVAL;
+    protected selectedFeatures: string[] = [];
 
     @Input() filter: IAtlasFilter = this.atlasFilterService.getTransitoryFilter();
     @Input() form = this.atlasFilterService.initializeFilterForm(this.filter);
 
+    ngOnInit(): void {
+        this.selectedFeatures = this.getSelectedFeatures();
+    }
+
     onDimensionChanges(dimension: IQoLIDimension): void {
         const dimKey = dimension.filename;
         const value = this.form.get(dimKey)?.value;
+        dimension.checked = value;
 
         for (const indicator of dimension.aggregators) {
             const indKey = `${dimKey}:${indicator.filename}`;
             this.form.get(indKey)?.setValue(value);
             indicator.checked = value;
         }
+
+        this.updateSelectedFeatures();
     };
 
     onIndicatorChanges(dimension: IQoLIDimension, indicator: IQoLIIndicator): void {
@@ -49,6 +57,16 @@ export class AtlasFilterMainSectionComponent {
         if (unchecked) {
             this.form.get(dimension.filename)?.setValue(false);
         }
+
+        this.updateSelectedFeatures();
+    }
+
+    onFeaturesChanges(event: MatSelectChange): void {
+        // Do nothing
+    }
+
+    onYearChanges(event: MatSelectChange): void {
+        this.form.get('year')?.setValue(event.value);
     }
 
     someChecked(dimension: IQoLIDimension): boolean {
@@ -62,7 +80,22 @@ export class AtlasFilterMainSectionComponent {
         return dimension.aggregators.some(indicator => indicator.checked);
     }
 
-    onYearChanges(event: MatSelectChange): void {
-        this.form.get('year')?.setValue(event.value);
+    getSelectedFeatures(): string[] {
+        const dimensions = this.filter.baseFilter.qoliOptions.aggregators;
+
+        return dimensions.reduce((acc, dimension) => {
+            dimension.checked && acc.push(dimension.filename);
+            const indicators = dimension.aggregators;
+
+            for (const indicator of indicators) {
+                indicator.checked && acc.push(indicator.filename);
+            }
+
+            return acc;
+        }, [] as string[]);
+    }
+
+    updateSelectedFeatures(): void {
+        this.selectedFeatures = this.getSelectedFeatures();
     }
 }
